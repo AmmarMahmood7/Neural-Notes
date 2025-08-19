837f695c757437c46b541989ad2069145a59db05
ammarmahmood7 <ammarmahmood97@gmail.com>
2025-08-19 22:47:19 +0530

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import DarkModeToggle from "./DarkModeToggle";
+import LogOutButton from "./LogOutButton";
+import { getUser } from "@/auth/server";
+import { SidebarTrigger } from "./ui/sidebar";
+
+async function Header() {
+  const user = await getUser();
+
+  return (
+    <header
+      className="relative flex h-24 w-full items-center justify-between bg-popover px-3 sm:px-8"
+    >
+      <SidebarTrigger className="absolute left-1 top-1" />
+
+      <Link className="flex items-center gap-2" href="/">
+        <Image
+          src="/Neural_logo.png"
+          height={100}
+          width={100}
+          alt="logo"
+          className="rounded-full"
+          priority
+        />
+
+        <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">
+          Neural
+        </h1>
+      </Link>
+
+      <div className="flex gap-4">
+        {user ? (
+          <LogOutButton />
+        ) : (
+          <>
+            <Button asChild>
+              <Link href="/sign-up" className="hidden sm:block">
+                Sign Up
+              </Link>
+            </Button>
+            <Button asChild variant="outline">
+              <Link href="/login">Login</Link>
+            </Button>
+          </>
+        )}
+        <DarkModeToggle />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
