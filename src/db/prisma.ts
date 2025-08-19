7be3444b4f0a1e7b9a16ae00b00679a529415fe6
ammarmahmood7 <ammarmahmood97@gmail.com>
2025-08-19 23:13:49 +0530

    configured for build

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,7 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  }).$extends({
+    query: {
+      $allOperations({ operation, args, query }) {
+        const start = Date.now();
+        return query(args).finally(() => {
+          const end = Date.now();
+          console.log(`${operation} took ${end - start}ms`);
+        });
+      },
+    },
+  });
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
